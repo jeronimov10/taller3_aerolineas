72f4648d27053af36223a8ca9f4a0e853e53139d
jeronimov10 <j.vasquezp2@uniandes.edu.co>
2024-09-03 20:37:43 -0500

    modificaciones cliente

--- src/uniandes/dpoo/aerolinea/modelo/cliente/Cliente.java
+++ src/uniandes/dpoo/aerolinea/modelo/cliente/Cliente.java
@@ -1,7 +1,54 @@
 package uniandes.dpoo.aerolinea.modelo.cliente;
-import uniandes.dpoo.aerolinea.modelo.Aerolinea;
+import java.util.ArrayList;
 
 
-public class Cliente  {
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+import uniandes.dpoo.aerolinea.tiquetes.Tiquete;
+
+
+public abstract class Cliente  {
+	
+	private java.util.List<Tiquete> tiquetesSinUsar;
+	private java.util.List<Tiquete> tiquetesUsados;
+	
+	
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+		
+		this.tiquetesSinUsar = new ArrayList<Tiquete>();
+		this.tiquetesUsados = new ArrayList<Tiquete>();
+		
+	}
+	public abstract java.lang.String getTipoCliente();
+	
+	public abstract java.lang.String getIdentificador();
+	
+	public void agregarTiquete​(Tiquete tiquete) {
+		tiquetesSinUsar.add(tiquete);
+		
+	}
+	
+	public void usarTiquetes(Vuelo vuelo) {
+		
+		tiquetesUsados.add(vuelo.getTiquetes());
+	}
+	
+	public int calcularValorTotalTiquetes() {
+		
+		return -1;
+	}
+	
+	
+	public java.util.List<Tiquete> getTiquetesSinUsar() {
+		return tiquetesSinUsar;
+	}
+	public java.util.List<Tiquete> getTiquetesUsados() {
+		return tiquetesUsados;
+	}
+	
+	
+	
+
 
 }
