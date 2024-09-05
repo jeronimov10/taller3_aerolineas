4ce4672d512ffb28fe482de766944b9e486b9be1
jeronimov10 <j.vasquezp2@uniandes.edu.co>
2024-09-05 08:35:20 -0500

    Cambios temporada alta y temporada baja

--- src/uniandes/dpoo/aerolinea/modelo/tarifas/calculadoraTemporadaAlta.java
+++ src/uniandes/dpoo/aerolinea/modelo/tarifas/calculadoraTemporadaAlta.java
@@ -1,5 +1,28 @@
 package uniandes.dpoo.aerolinea.modelo.tarifas;
 
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+import uniandes.dpoo.aerolinea.modelo.cliente.Cliente;
+
 public class calculadoraTemporadaAlta extends calculadoraTarifas{
+	
+	protected int COSTO_POR_KM = 1000;
+	
+	
+	
+	public int calcularCostoBase(Vuelo vuelo, Cliente cliente) {
+		
+		return -1;
+	}
+	
+	public double calcularPocentajeDescuento(Cliente cliente) {
+		
+		
+		return 0.0;
+	}
+	
+	
+	public calculadoraTemporadaAlta() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/uniandes/dpoo/aerolinea/modelo/tarifas/calculadoraTemporadaBaja.java
+++ src/uniandes/dpoo/aerolinea/modelo/tarifas/calculadoraTemporadaBaja.java
@@ -1,5 +1,31 @@
 package uniandes.dpoo.aerolinea.modelo.tarifas;
 
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+import uniandes.dpoo.aerolinea.modelo.cliente.Cliente;
+
 public class calculadoraTemporadaBaja extends calculadoraTarifas{
+	
+	protected int COSTO_POR_KM_NATURAL = 600;
+	protected int COSTO_POR_KM_CORPORATIVO = 900;
+	protected double DESCUENTO_PEQ = 0.02;
+	protected double DESCUENTO_MED = 0.1;
+	protected double DESCUENTO_GRANDES = 0.2;
+	
+	private int calcularCostoBase(Vuelo vuelo, Cliente cliente) {
+		
+		return -1;
+	}
+	
+	private double calcularporcentajeDescuento(Cliente cliente) {
+		
+		
+		return 0.0;
+	}
+	
+	public calculadoraTemporadaBaja() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
